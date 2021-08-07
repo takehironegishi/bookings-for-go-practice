d7cf314b6e6698d82d78cfeed273bfdf57bbd6e6
takehironegishi <takehiro.n61@gmail.com>
2021-08-07 23:47:24 +0900

    connect current directories to remote repository

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bookings-for-go-practice
+
+- This repo is just for practice of go
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,52 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/takehironegishi/go-practice/pkg/config"
+	"github.com/takehironegishi/go-practice/pkg/models"
+	"github.com/takehironegishi/go-practice/pkg/render"
+)
+
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is the home page handler
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "home.page.gohtml", &models.TemplateData{})
+}
+
+// About is the about page handler
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again."
+
+	remoteIp := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIp
+
+	// send the data to the template
+	render.RenderTemplate(w, "about.page.gohtml", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
